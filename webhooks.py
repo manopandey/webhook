a1b1de4a935ca2df7b541dfad669ea99545d5a29
Manoranjan Pandey <manoranjan.pandey@justanalytics.com>
2018-08-31 11:19:51 +0800

    testing git url

--- webhooks.py
+++ webhooks.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def webhook():
-    print(request.json['repository']['name'])
+    print("https://github.com/"+request.json['repository']['full_name'])
     #identify which repo
     #do a git pull in specified repo
     #run pm2 deploy script in repo
