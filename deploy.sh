c3facc68afc2cced75661eec6d50207a0ef64f17
Manoranjan Pandey <manoranjan.pandey@justanalytics.com>
2018-08-31 11:50:10 +0800

    test if automatic deployment works

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+sudo git pull
+sudo pm2 restart Webhook
--- webhooks.py
+++ webhooks.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 def webhook():
     repo_url = request.json['repository']['clone_url']
     print(repo_url)
-    call(["sudo git pull", "/var/www/html/"+request.json['repository']['name']])
-    call(["sudo pm2 restart Webhook"])
+
     return Response(status=202, mimetype='application/json')
 
 
