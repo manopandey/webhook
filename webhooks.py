2e6e68e4a0edcbb74dc5760b9492aeb37474dced
Your Name <mano191091@gmail.com>
2018-08-31 07:23:40 +0000

    change shell call command

--- webhooks.py
+++ webhooks.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, Response
-from subprocess import call
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def webhook():
+    os.system('( cd /var/www/html/'request.json['repository']['name']' ; sudo git pull )')
     return(str(request.json['repository']['clone_url']))
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
-    
-#test
