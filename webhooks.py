2e1b7dc1086ee139abaa62fcde1b22eb6836ae26
Manoranjan Pandey <manoranjan.pandey@justanalytics.com>
2018-08-31 10:43:14 +0800

    pusing to server

--- webhooks.py
+++ webhooks.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def webhook():
-    print(request.Payload)
+    print(request.json.repository.name)
     return Response(status=202, mimetype='application/json')
 
 
